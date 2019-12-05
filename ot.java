0b88477438d52bb8709b859dd97c05e5034846f1
mickykrish007 <58469412+mickykrish007@users.noreply.github.com>
2019-12-05 16:29:19 +0530

    Add files via upload

---
+++ ot.java
@@ -0,0 +1,9 @@
+class ot
+{
+public static void main(String[] args) {
+  int i;
+  for (i=1;i<=10 ;i++ ) {
+    System.out.print(i+"\t");
+  }
+}
+}
---
+++ sortalpha.java
@@ -0,0 +1,14 @@
+class sortalpha
+{
+public static void main(String[] args) {
+  char a='a',b='e';
+  if (a<b) {
+    System.out.print(a+","+b);
+  }
+  else
+  {
+    System.out.print(b+","+a);
+  }
+}
+
+}
